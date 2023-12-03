fa86e5a08c9458bcb8853ffd6293e227d98f0622
Eaindray <eaindrayfromearth@gmail.com>
2023-12-03 18:34:28 +1400

    Add project files.

---
+++ WebApp.Report/wwwroot/reports/ReportDataSet.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp.Report.wwwroot.reports
+{
+
+
+    partial class ReportDataSet
+    {
+        partial class Tbl_BlogDataTable
+        {
+
+            /*            public int Blog_Id { get; set; }
+                        public string Blog_Title { get; set; }
+                        public string Blog_Author { get; set; }
+                        public string Blog_Content { get; set; }*/
+        }
+    }
+}
---
+++ WindowsFormsApp.Report/ReportDataSet.cs
@@ -0,0 +1,16 @@
+﻿namespace WindowsFormsApp.Report
+{
+
+
+    partial class ReportDataSet
+    {
+        partial class Tbl_BlogDataTable
+        {
+
+            public int Blog_Id { get; set; }
+            public string Blog_Title { get; set; }
+            public string Blog_Author { get; set; }
+            public string Blog_Content { get; set; }
+        }
+    }
+}
